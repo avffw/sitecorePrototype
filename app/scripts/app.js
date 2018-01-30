67932937732dde18e3ad66da478dff8d00a0e99a
AV <andrei.verdes@ffwagency.com>
2018-01-30 09:23:06 +0100

    error functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -807,39 +807,66 @@ $(function () {
     theme.form = {
         elements: {
             form: $('form'),
-            nameField: $("input[name='name']"),
-            emailField: $("input[name='email']"),
-            subjectField: $("input[name='subject']"),
-            textArea: $('textarea[name="message"]'),
-            submitButton: $('div[data-name="submit"')
+            fields: {
+                nameField: $("input[name='name']"),
+                emailField: $("input[name='email']"),
+                subjectField: $("input[name='subject']"),
+                textArea: $("textarea[name='message']"),
+
+            },
+            submitButton: $("div[data-name='submit']")
+
         },
         init: function () {
 
 
             theme.form.elements.submitButton.click(function () {
 
-                theme.form.validateFields(theme.form.elements);
+                theme.form.processFields(theme.form.elements.fields);
+
 
             });
 
         },
-        validateFields: function (fieldsObj) {
+        processFields: function (fieldsObj) {
 
             var verified = false;
+            var fieldsArray = [];
+            var emptyFields = [];
+            fieldsArray = $.map(fieldsObj, function (value, index) {
+                return value;
+            });
+
+            for (var i = 0; i < fieldsArray.length; i += 1){
+                if(!fieldsArray[i].val()){
+                    emptyFields.push(fieldsArray[i]);
+                }
+            }
+
+
+                if (!emptyFields.length) {
+                    verified = true;
+
+                } else {
+
+                    theme.form.throwError(emptyFields);
+                }
+
+
             theme.form.submitForm(verified);
 
         },
         submitForm: function (valid) {
-            var invalidFields;
-            if (!valid) {
 
-                theme.form.throwError(invalidFields);
-            } else {
+            if (valid) {
                 theme.form.elements.form.submit();
-            }
-        },
-        throwError: function () {
 
+        }
+        },
+
+
+        throwError: function (emptyFields) {
+            alert('error')
         }
 
     }
